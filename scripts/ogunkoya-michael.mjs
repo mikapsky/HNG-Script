7d0dcb322387d6ec45a8226ce0619e5731a2bf41
mikapsky <63076984+mikapsky@users.noreply.github.com>
2020-06-03 22:05:32 +0100

    Update ogunkoya-michael.mjs

--- scripts/ogunkoya-michael.mjs
+++ scripts/ogunkoya-michael.mjs
@@ -1,4 +1,5 @@
-let jay = '"Hello world this is ';
-let joy = 'Michael Ogunkoya"';
+
+let joy = 'Michael Ogunkoya';
 let jey = 'using javascript for stage 2 task."';
-console.log(` ${jay} ${joy} with HNGi7 ID [00486] ${jey}`);
+let jay='"Hello world this is';
+console.log(`${jay} ${joy} with HNGi7 ID [00486] ${jey}`);
